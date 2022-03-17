ed11fc7f6e789ed547311f4a2c7c5d11b6944004
Petar <p.hrusanov@gmail.com>
2022-03-17 17:55:32 +0200

    Updated Error Dictionary

---
+++ BrandexSalesAdapter.ExcelLogic/Data/Seeding/CitySeeder.cs
@@ -0,0 +1,13 @@
+namespace BrandexSalesAdapter.ExcelLogic.Data.Seeding
+{
+    using System;
+    using System.Threading.Tasks;
+    
+    public class CitySeeder : ISeeder
+    {
+        public Task SeedAsync(SpravkiDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
