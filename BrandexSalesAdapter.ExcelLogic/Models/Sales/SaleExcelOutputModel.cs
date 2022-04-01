33eba19d0f157414649cbca7a4c40d57fc4ec656
Petar <p.hrusanov@gmail.com>
2022-04-01 15:25:09 +0300

    Sales Logic Expanded with Summed and Separated Sales + Extra Logic Abstraction

--- BrandexSalesAdapter.ExcelLogic/Models/Sales/SaleExcelOutputModel.cs
+++ BrandexSalesAdapter.ExcelLogic/Models/Sales/SaleExcelOutputModel.cs
@@ -1,14 +1,21 @@
-﻿using System;
-namespace BrandexSalesAdapter.ExcelLogic.Models.Sales
+﻿namespace BrandexSalesAdapter.ExcelLogic.Models.Sales
 {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using BrandexSalesAdapter.ExcelLogic.Models.Products;
+    
     public class SaleExcelOutputModel
     {
         public string Name { get; set; }
 
         public int ProductId { get; set; }
+        
+        public double ProductPrice { get; set; }
 
         public int Count { get; set; }
 
         public DateTime? Date { get; set; }
+        
     }
 }
