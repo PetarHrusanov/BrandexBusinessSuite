68777b4db6883db2b2fddf75148cef3bc0646552
Petar <p.hrusanov@gmail.com>
2022-05-16 22:09:50 +0300

    Logic for Dowloading Accounting xls from PDF file

--- BrandexSalesAdapter.Accounting/Controllers/FacebookCountingController.cs
+++ BrandexSalesAdapter.Accounting/Controllers/FacebookCountingController.cs
@@ -1,16 +1,22 @@
 namespace BrandexSalesAdapter.Accounting.Controllers;
 
 using System.IO;
-
 using BrandexSalesAdapter.Controllers;
-using BrandexSalesAdapter.Infrastructure;
-using BrandexSalesAdapter.Models;
+
+using Infrastructure;
+
+using static Common.ProductConstants;
+
+using System.Text.RegularExpressions;
+
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using iTextSharp.text.pdf;
 using Microsoft.AspNetCore.Mvc;
-
 using iTextSharp.text.pdf.parser;
-
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
 
 
 public class FacebookCountingController : ApiController
@@ -24,34 +30,80 @@ public class FacebookCountingController : ApiController
     }
 
     [HttpPost]
+    [AllowAnonymous]
+    [IgnoreAntiforgeryToken]
     [Consumes("multipart/form-data")]
-    public async Task<string> Convert([FromForm] IFormFile file)
+    public async Task<IActionResult> Convert([FromForm] IFormFile file)
     {
+
+        double euroRate = 1.9894;
 
         string newPath = CreateFileDirectories.CreatePDFFilesInputDirectory(_hostEnvironment);
 
-        var errorDictionary = new Dictionary<int, string>();
+        if (file.Length <= 0) throw new ArgumentNullException();
+        
+        var fullPath = System.IO.Path.Combine(newPath, file.FileName);
 
-        if (file.Length <= 0) return "Error";
-        var sFileExtension = System.IO.Path.GetExtension(file.FileName)?.ToLower();
+        await using var streamRead = new FileStream(fullPath, FileMode.Create);
+        await file.CopyToAsync(streamRead);
+
+        string rawText = PdfText(fullPath);
+
+        var productsPrices = ProductPriceDictionaryFromText(rawText);
+        
+        var sWebRootFolder = _hostEnvironment.WebRootPath;
+        var sFileName = @"Facebook_Accounting.xlsx";
+
+        var memory = new MemoryStream();
+        
+        RenameKey(productsPrices, Facebook.ZinSeD, ERP_Accounting.ZinSeD);
+        RenameKey(productsPrices, Facebook.EnzyMill, ERP_Accounting.EnzyMill);
+        RenameKey(productsPrices, Facebook.CystiRen, ERP_Accounting.CystiRen);
+        RenameKey(productsPrices, Facebook.LadyHarmonia, ERP_Accounting.LadyHarmonia);
+        RenameKey(productsPrices, Facebook.LaxaL, ERP_Accounting.LaxaL);
+        RenameKey(productsPrices, Facebook.Bland, ERP_Accounting.Bland);
+        RenameKey(productsPrices, Facebook.DiabeForGluco, ERP_Accounting.DiabeForGluco);
+        RenameKey(productsPrices, Facebook.GinkgoVin, ERP_Accounting.GinkgoVin);
+        RenameKey(productsPrices, Facebook.Venaxin, ERP_Accounting.Venaxin);
+        RenameKey(productsPrices, Facebook.ForFlex, ERP_Accounting.ForFlex);
+        RenameKey(productsPrices, Facebook.ProstaRen, ERP_Accounting.ProstaRen);
+        RenameKey(productsPrices, Facebook.Sleep, ERP_Accounting.Sleep);
+        RenameKey(productsPrices, Facebook.DetoxiFive, ERP_Accounting.DetoxiFive);
+        RenameKey(productsPrices, Facebook.GeneralAudience, ERP_Accounting.Botanic);
+        
+        var sortedProducts = productsPrices.OrderBy(x => x.Key);
+
+        await using (var fs = new FileStream(System.IO.Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
         {
-            var fullPath = System.IO.Path.Combine(newPath, file.FileName);
+            IWorkbook workbook = new XSSFWorkbook();
 
-            await using var stream = new FileStream(fullPath, FileMode.Create);
-            await file.CopyToAsync(stream);
+            var excelSheet = workbook.CreateSheet("Products Summed");
+
+            var row = excelSheet.CreateRow(0);
+
+            foreach (var dictEntry in sortedProducts)
+            {
+                row = excelSheet.CreateRow(excelSheet.LastRowNum+1);
+                row.CreateCell(row.Cells.Count()).SetCellValue(dictEntry.Key);
+                row.CreateCell(row.Cells.Count()).SetCellValue((double)dictEntry.Value);
+                row.CreateCell(row.Cells.Count()).SetCellValue((double)dictEntry.Value*euroRate);
+            }
+
+            workbook.Write(fs);
 
         }
 
+        await using (var streatWrite = new FileStream(System.IO.Path.Combine(sWebRootFolder, sFileName), FileMode.Open))
+        {
 
+            await streatWrite.CopyToAsync(memory);
 
-        var kur =
-            "/Users/Petar/Documents/Documents – Petar’s MacBook Pro/Programiranka/Firmata/BrandexSalesAdapter/Server/BrandexSalesAdapter/BrandexSalesAdapter.Accounting/wwwroot/UploadExcel/2022-05-10T07-08 Transaction #5018559291594644-9749365.pdf";
+        }
 
-        return PdfText(kur);
+        memory.Position = 0;
+
+        return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sFileName);
         
-               
-
-        return PdfText(newPath+sFileExtension);
     }
 
     private static string PdfText(string path)
@@ -61,9 +113,72 @@ public class FacebookCountingController : ApiController
         for(int page = 1; page <= reader.NumberOfPages; page++)
         {
             text += PdfTextExtractor.GetTextFromPage(reader,page);
+            text += Environment.NewLine;
         }
         reader.Close();
         return text;
-    }   
+    }
+
+    private Dictionary<string, decimal> ProductPriceDictionaryFromText(string rawText)
+    {
+        Regex priceRegex = new Regex(@"[0-9]*\.[0-9]*");
+
+        Dictionary<string, decimal> productsPrices = new Dictionary<string, decimal>();
+
+        var rawTextSplit = rawText.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        var products = new string[]
+        {
+            Facebook.ZinSeD,
+            Facebook.EnzyMill,
+            Facebook.CystiRen,
+            Facebook.LadyHarmonia,
+            Facebook.LaxaL,
+            Facebook.Bland,
+            Facebook.DiabeForGluco,
+            Facebook.GinkgoVin,
+            Facebook.Venaxin,
+            Facebook.ForFlex,
+            Facebook.ProstaRen,
+            Facebook.Sleep,
+            Facebook.DetoxiFive,
+            Facebook.GeneralAudience
+        };
+
+        foreach (var product in products)
+        {
+            // if (!rawTextSplit.Contains(product)) continue;
+            
+            var lines = rawTextSplit
+                .Where(element => element.Contains(product)).ToList();
+
+            if (lines.Count==0) continue;
+            
+            foreach (var line in lines)
+            {
+                if (!productsPrices.ContainsKey(product))
+                {
+                    productsPrices.Add(product,0);
+                }
+
+                var price = decimal.Parse(priceRegex.Matches(line)[0].ToString());
+
+                productsPrices[product] += price;
+            }
+
+        }
+
+        return productsPrices;
+
+    }
+    
+    public static void RenameKey<TKey, TValue>(IDictionary<TKey, TValue> dic,
+        TKey fromKey, TKey toKey)
+    {
+        TValue value = dic[fromKey];
+        dic.Remove(fromKey);
+        dic[toKey] = value;
+    }
+    
     
 }
---
+++ BrandexSalesAdapter/Common/ProductConstants.cs
@@ -0,0 +1,46 @@
+namespace BrandexSalesAdapter.Common;
+
+public class ProductConstants
+{
+    public class Facebook
+    {
+        public const string ZinSeD = "ZinSeD";
+        public const string EnzyMill = "Enzy-Mill";
+        public const string CystiRen = "CystiRen";
+        public const string LadyHarmonia = "LadyHarmonia";
+        public const string DetoxiFive = "DetoxiFive";
+        public const string LaxaL = "LaxaL";
+        public const string Bland = "Bland";
+        public const string DiabeForGluco = "DiabeFor Gluco";
+        public const string GinkgoVin = "GinkgoVin";
+        public const string Venaxin = "Venaxin";
+        public const string ForFlex = "ForFlex";
+        public const string ProstaRen = "ProstaRen";
+        public const string Sleep = "Sleep";
+        
+        public const string GeneralAudience = "General Audience";
+
+    }
+    
+    public class ERP_Accounting
+    {
+        public const string ZinSeD = "ЗинСеД";
+        public const string EnzyMill = "Ензи-Мил";
+        public const string CystiRen = "Цистирен";
+        public const string LadyHarmonia = "Лейди Хармония";
+        public const string DetoxiFive = "ДетоксиФайв";
+        public const string LaxaL = "Лаксал (псилиум)";
+        public const string Bland = "Бланд 30 табл.";
+        public const string DiabeForGluco = "ДиабеФор Глюко";
+        public const string GinkgoVin = "Гинко Вин";
+        public const string Venaxin = "Венаксин";
+        public const string ForFlex = "Форфлекс";
+        public const string ProstaRen = "ПростаРен";
+        public const string Sleep = "Слийп 30 табл.";
+        
+        public const string Botanic = "Ботаник";
+
+    }
+    
+    
+}
