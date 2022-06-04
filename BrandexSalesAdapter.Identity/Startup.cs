6d6311fb4d03e12313a744311e3088f66f000e4a
Petar <p.hrusanov@gmail.com>
2022-06-04 20:11:09 +0300

    Seeding Logic Added

--- BrandexSalesAdapter.Identity/Startup.cs
+++ BrandexSalesAdapter.Identity/Startup.cs
@@ -1,39 +1,42 @@
-namespace BrandexSalesAdapter.Identity
+using BrandexSalesAdapter.Services.Data;
+
+namespace BrandexSalesAdapter.Identity;
+
+using Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Identity;
+using Data.Models;
+using BrandexSalesAdapter.Infrastructure;
+using BrandexSalesAdapter.Identity.Data.Seeding;
+
+
+public class Startup
 {
-    using Data;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Services.Identity;
-    using Data.Models;
-    using BrandexSalesAdapter.Infrastructure;
+    public Startup(IConfiguration configuration) => this.Configuration = configuration;
 
+    private IConfiguration Configuration { get; }
 
-    public class Startup
+    public void ConfigureServices(IServiceCollection services)
     {
-        public Startup(IConfiguration configuration) => this.Configuration = configuration;
 
-        private IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddWebService<ApplicationUsersDbContext>(Configuration);
+        services.AddWebService<ApplicationUsersDbContext>(Configuration);
                 
-            services.AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
-                .AddRoles<ApplicationRole>().AddEntityFrameworkStores<ApplicationUsersDbContext>();
+        services.AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
+            .AddRoles<ApplicationRole>().AddEntityFrameworkStores<ApplicationUsersDbContext>();
 
-            services
-                .AddTransient<IIdentityService, IdentityService>()
-                .AddTransient<ITokenGeneratorService, TokenGeneratorService>();
+        services
+            .AddTransient<ISeeder, ApplicationDbContextSeeder>()
+            .AddTransient<IIdentityService, IdentityService>()
+            .AddTransient<ITokenGeneratorService, TokenGeneratorService>();
             
-        }
+    }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseWebService(env)
-                .Initialize();
-        }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseWebService(env)
+            .Initialize();
     }
 }
